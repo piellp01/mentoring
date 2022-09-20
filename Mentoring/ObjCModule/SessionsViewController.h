801b749f056f6ad75ac1afae135df1dba32e427b
Emma Walker <walkere@tcd.ie>
2022-09-20 13:38:08 +0100

    adds an empty table view

--- Mentoring/ObjCModule/SessionsViewController.h
+++ Mentoring/ObjCModule/SessionsViewController.h
@@ -1,5 +1,5 @@
 #import <UIKit/UIKit.h>
 
-@interface SessionsViewController: UIViewController
+@interface SessionsViewController: UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
--- Mentoring/ObjCModule/SessionsViewController.m
+++ Mentoring/ObjCModule/SessionsViewController.m
@@ -4,7 +4,19 @@
 
 - (void)viewDidLoad
 {
-    self.view.backgroundColor = [UIColor redColor];
+    UITableView *tableView = [[UITableView alloc] init];
+    tableView.delegate = self;
+    tableView.dataSource = self;
 }
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return [[UITableViewCell alloc] init];
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+
 
 @end
